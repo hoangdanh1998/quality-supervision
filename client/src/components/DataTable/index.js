9dc2c095693d333db09027449389d42655969748
hoanggia <hoanggia1224@gmail.com>
2022-11-03 12:44:49 +0700

    [UI] Fix table data remove and add function error

--- client/src/components/DataTable/index.js
+++ client/src/components/DataTable/index.js
@@ -1,59 +1,99 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { DataTable as Table } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import _ from 'lodash'
 import './style.scss'
 
+let n = 0
 const columns = [
     { field: 'key', header: 'Key' },
     { field: 'value', header: 'Value' },
     { field: 'comment', header: 'Comment' },
-    { field: 'actions'}
 ];
+
+const newEmptyParam = { key: "", value: "", comment: "" }
+
 const DataTable = () => {
-    const [products2, setProducts2] = useState([{key: "aa", value: "", comment: "", actions: ""}]);
-    const [selectedCustomer,setSelectedCustomer] = useState(null)
+
+
+    const [products2, setProducts2] = useState([newEmptyParam]);
+    const [selectedCustomer, setSelectedCustomer] = useState(null)
+
     const onCellEditComplete = (e) => {
         let { rowData, newValue, field, originalEvent: event } = e;
 
-        switch (field) {
-            case 'quantity':
-            case 'price':
-             break;
+        if (newValue.trim().length <= 0 && field === 'key') return
+        rowData[field] = newValue;
+        if (products2[products2.length - 1].key)
+            setProducts2([...products2, { key: "", value: "", comment: "" }])
 
-            default:
-                if (newValue.trim().length > 0)
-                    rowData[field] = newValue;
-                else
-                    event.preventDefault();
-                break;
-        }
     }
 
-    const cellEditor = (options) => {
-        if (options.field === 'actions')
-            return;
-        else
-            return textEditor(options);
-      }
-      
-      const textEditor = (options) => {
-  
-        return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
-      }
+    const cellEditor = (option) => {
+        debugger
+        return (
+            <InputText
+                type="text"
+                //value={option?.value}
+                value={option.rowData[option?.value]}
+                onChange={(e) => option.editorCallback(e.target.value)}
+            />
+        )
+        // if (option.field === 'action')
+        //     return;
+        // else {
+        //     return textEditor(option);
 
-  return (
-    <div className="card p-fluid">
-        <h5>Row Editing</h5>
-        <Table value={products2} editMode="cell" className="editable-cells-table" responsiveLayout="scroll">
-        {
-            columns.map(({ field, header }) => {
-                            return <Column key={field} field={field} header={header} style={{ width: '25%' }} body={field === 'price'}
-                                editor={(options) => cellEditor(options)} onCellEditComplete={onCellEditComplete} />
-            })
-        }
-        </Table>
-    </div>
-  )
+        // }
+    }
+
+    // const textEditor = (options) => {
+    //     console.log('options: ', options)
+
+    //     return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
+    // }
+    // let times = 0;
+    const onClickRemoveButton = rowData => {
+        let newList = [...products2]
+        _.remove(newList, item => item.key === rowData.key)
+        setProducts2(newList)
+        console.log(newList);
+
+        if (products2.length <= 0)
+            setProducts2([{ key: "", value: "", comment: "" }])
+    }
+
+    const actionBodyTemplate = rowData => {
+        if (_.some(Object.values(rowData), item => !!item))
+            return (
+                <Button type="button"
+                    className="p-button-rounded p-button-text"
+                    icon="pi pi-trash"
+                    onClick={() => onClickRemoveButton(rowData)} />
+            )
+    }
+
+    return (
+        <div className="card p-fluid">
+            <h5>Header</h5>
+            <Table
+                value={products2}
+                editMode="cell"
+                responsiveLayout="scroll"
+                scrollable scrollHeight="12rem"
+                className="editable-cells-table"
+            >
+                {columns.map(({ field, header }) => (
+                    <Column key={field} field={field} header={header} style={{ width: '25%' }}
+                        editor={(options) => cellEditor(options)} onCellEditComplete={onCellEditComplete} />
+                ))}
+                <Column style={{ width: '25%' }}    // key="action"
+                    body={(rowValues, rowData) => actionBodyTemplate(rowValues, rowData)}
+                    bodyStyle={{ display: 'flex', justifyContent: 'flex-end' }} />
+            </Table>
+        </div>
+    )
 }
 export default DataTable;
--- client/src/components/RequestBox/index.js
+++ client/src/components/RequestBox/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { BreadCrumb } from 'primereact/breadcrumb';
 import { InputText } from 'primereact/inputtext';
 import { Dropdown } from 'primereact/dropdown';
@@ -7,7 +7,7 @@ import { TabMenu } from 'primereact/tabmenu';
 import { RadioButton } from 'primereact/radiobutton';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { InputNumber } from 'primereact/inputnumber';
-import {DataTable} from '../../components'
+import { DataTable } from '../../components'
 import styles from './RequestBox.module.scss'
 import clsx from 'clsx';
 
@@ -15,6 +15,7 @@ const items = [
   { label: 'Blog Application' },
   { label: 'List Blog' },
 ];
+
 const cities = [
   'GET',
   'POST',
@@ -22,13 +23,13 @@ const cities = [
   'PACTH',
   'PUT'
 ];
-const home = { icon: 'pi pi-th-large'}
+const home = { icon: 'pi pi-th-large' }
 const contentTypes = [
-  {name: 'application/json'},
-  {name: 'application/text'},
-  {name: 'application/html'},
+  'application/json',
+  'application/text',
+  'application/html',
 ];
-const frameworks = [{name: "Jest"},{name: "Junit"}]
+const frameworks = ["Jest", "Junit"]
 
 
 function RequestBox() {
@@ -37,91 +38,94 @@ function RequestBox() {
   const [bodyType, setBodyType] = useState(null);
   const [code, setCode] = useState("")
   const [statusCode, setStatusCode] = useState("")
-  const [contentType, setContentType] = useState({name: "application/json"})
+  const [contentType, setContentType] = useState("application/json")
 
   const [framework, setFramework] = useState("Jest")
   const tabs = [
-    {label: 'Params'},
-    {label: 'Auth'},
-    {label: 'Header'},
-    {label: 'Body'},
-    {label: 'Expected Output'},
-    {label: 'Jest', 
-    template: (item, options) => {
-      return (
+    { label: 'Params' },
+    { label: 'Auth' },
+    { label: 'Header' },
+    { label: 'Body' },
+    { label: 'Expected Output' },
+    {
+      label: 'Jest',
+      template: (item, options) => {
+        return (
           /* custom element */
-          <Dropdown style={{border: 'none'}} optionLabel="name" value={framework} options={frameworks} onChange={(e) => setFramework(e.value)}/>
-      );
-  }},
+          <Dropdown style={{ border: 'none' }}
+            value={framework}
+            options={frameworks}
+            onChange={(e) => setFramework(e.value)} />
+        );
+      }
+    },
   ];
 
   let tabContent;
   switch (activeIndex) {
     case 0: break;
-    case 2: 
-    tabContent = <DataTable/>;
-    break;
+    case 2:
+      tabContent = <DataTable />; break;
     case 1: break;
-    case 3: 
-      tabContent = 
-    <div className='mt-3 '>
-      <div className='flex md:justify-content-start justify-content-center flex-wrap card-container cyan-container'>
-        <div className="field-radiobutton mr-5">
+    case 3:
+      tabContent =
+        <div className='mt-3 '>
+          <div className='flex md:justify-content-start justify-content-center flex-wrap card-container cyan-container'>
+            <div className="field-radiobutton mr-5">
               <RadioButton value="val1" name="city1" onChange={(e) => setBodyType(e.value)} checked={bodyType === 'val1'} />
               <label htmlFor="city1">raw</label>
-        </div>
-        <div className="field-radiobutton  mr-5">  
+            </div>
+            <div className="field-radiobutton  mr-5">
               <RadioButton value="val2" name="city2" onChange={(e) => setBodyType(e.value)} checked={bodyType === 'val2'} />
               <label htmlFor="city2">form-data</label>
-        </div>
-        <div className="field-radiobutton  mr-5">  
+            </div>
+            <div className="field-radiobutton  mr-5">
               <RadioButton value="val3" name="city3" onChange={(e) => setBodyType(e.value)} checked={bodyType === 'val3'} />
               <label htmlFor="city3">x-www-form-unlencoded</label>
-        </div>
-      </div>
-      <div>
-              <InputTextarea rows={5} 
-              value={code} 
-              style={{width: '100%'}}
+            </div>
+          </div>
+          <div>
+            <InputTextarea rows={5}
+              value={code}
+              style={{ width: '100%' }}
               onChange={(e) => setCode(e.target.value)} />
-      </div>
-    </div>
-    break;
-    case 4: 
-    tabContent =  
-    <>
-    <div className='mt-3 mb-3 flex justify-content-between'>
-      <div className='flex align-items-center'>
-        <div className='mr-2'>Status Code</div>
-        <InputNumber 
-        className={clsx(styles.p_inputnumber, {
-          [styles.accept]: statusCode >= 200 && statusCode < 300,
-          [styles.request_error]: statusCode >= 400 && statusCode < 500,
-          [styles.server_error]: statusCode >= 500 && statusCode < 600
-        })}
-         value={statusCode} 
-         onChange={(e) => setStatusCode(e.value)} />
-      </div>
-      <div className='flex align-items-center'>
-        <div className='mr-2'>Content-Type</div>
-        <Dropdown 
-          className={styles.p_dropdown} 
-          optionLabel="name" 
-          value={contentType}
-          options={contentTypes} 
-          onChange={(e) => setContentType(e.value)}
-         />
-      </div>
-    </div>
-    <div>
-        <InputTextarea rows={5} 
-        value={code} 
-        style={{width: '100%'}}
-        onChange={(e) => setCode(e.target.value)}
-        placeholder="Response "/>
-    </div>
-    </>
-    break;
+          </div>
+        </div>
+      break;
+    case 4:
+      tabContent =
+        <>
+          <div className='mt-3 mb-3 flex justify-content-between'>
+            <div className='flex align-items-center'>
+              <div className='mr-2'>Status Code</div>
+              <InputNumber
+                className={clsx(styles.p_inputnumber, {
+                  [styles.accept]: statusCode >= 200 && statusCode < 300,
+                  [styles.request_error]: statusCode >= 400 && statusCode < 500,
+                  [styles.server_error]: statusCode >= 500 && statusCode < 600
+                })}
+                value={statusCode}
+                onChange={(e) => setStatusCode(e.value)} />
+            </div>
+            <div className='flex align-items-center'>
+              <div className='mr-2'>Content-Type</div>
+              <Dropdown
+                className={styles.p_dropdown}
+                value={contentType}
+                options={contentTypes}
+                onChange={(e) => setContentType(e.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <InputTextarea rows={5}
+              value={code}
+              style={{ width: '100%' }}
+              onChange={(e) => setCode(e.target.value)}
+              placeholder="Response " />
+          </div>
+        </>
+      break;
     default:
       break;
   }
@@ -129,19 +133,19 @@ function RequestBox() {
   return (
     <div className='px-1'>
       <h3>
-          <BreadCrumb model={items} home={home} style={{background: 'inherit', border: 'none'}}/>
+        <BreadCrumb model={items} home={home} style={{ background: 'inherit', border: 'none' }} />
       </h3>
       <div className='w-full flex mt-2'>
-          <Dropdown className='w-25' value={city} options={cities} onChange={(e) => setCity(e)}
-          style={{minWidth: "7.5rem",}}/>
-          <div className="p-inputgroup mr-2 ml-2">
-              <InputText placeholder="Enter request URL" />
-          </div>
-          <Button label="GO" />
+        <Dropdown className='w-25' value={city} options={cities} onChange={(e) => setCity(e.value)}
+          style={{ minWidth: "7.5rem", }} />
+        <div className="p-inputgroup mr-2 ml-2">
+          <InputText placeholder="Enter request URL" />
+        </div>
+        <Button label="GO" />
       </div>
-      <TabMenu className='mt-2' model={tabs} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}/>
-      { tabContent}
-  </div>
+      <TabMenu className='mt-2' model={tabs} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
+      {tabContent}
+    </div>
   )
 }
 
